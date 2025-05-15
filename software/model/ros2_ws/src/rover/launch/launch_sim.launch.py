86a0735a40fb3243f15e3028b3805f855edf5823
yoshidog2005 <brandotl@icloud.com>
2025-05-15 12:04:10 -0700

    The file changes for jazzy->humble, untested as my humble gazebo does not work yet

--- software/model/ros2_ws/src/rover/launch/launch_sim.launch.py
+++ software/model/ros2_ws/src/rover/launch/launch_sim.launch.py
@@ -23,14 +23,12 @@ def generate_launch_description():
 
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
         ]),
         launch_arguments={'gz_args': ['-v 4', '-r empty.sdf']}.items()
     )
 
-
-
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
